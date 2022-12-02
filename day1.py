ee40fb2bf0411658ad9b9f609d78725598a62a11
feeger <emma.feege@gmail.com>
2022-12-02 14:38:16 +0100

    added part 2 of day 1

--- day1.py
+++ day1.py
@@ -4,10 +4,14 @@ from typing import List
 def GetInput(input: pathlib.Path) -> List[List[int]]:
   return [list(map(int, block.split("\n"))) for block in input.read_text().split("\n\n")[:-1]]
 
-def GetMostCalories() -> int:
-  allCalories = [sum(elfCalories) for elfCalories in GetInput(pathlib.Path("puzzle-input\input_day1.txt"))]
-  return max(allCalories)
+def GetElfWithMostCalories(caloriesList: List[List[int]]) -> int:
+  caloriesSums = [sum(elfCalories) for elfCalories in caloriesList]
+  return max(caloriesSums)
   
+def GetCaloriesSumOfTopThreeElves(caloriesList: List[List[int]]) -> int:
+  return sum(sorted([sum(elfCalories) for elfCalories in caloriesList])[-3:])
 
 if __name__ == '__main__':
-  print(GetMostCalories())
+  elvesCalories = GetInput(pathlib.Path("puzzle-input\input_day1.txt"))
+  print(f"{GetElfWithMostCalories(elvesCalories)=}")
+  print(f"{GetCaloriesSumOfTopThreeElves(elvesCalories)=}")
