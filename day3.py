9c75fbac43ec50ed0e065864dd462dbeba02839d
feeger <emma.feege@gmail.com>
2022-12-03 16:13:14 +0100

    added solution for part 2 of day 3

--- day3.py
+++ day3.py
@@ -4,12 +4,21 @@ from typing import List
 def GetCommonLetterPriority(backpack: str) -> int:
   firstCompartment = backpack[:int(len(backpack)/2)]
   secondCompartment = backpack[int(len(backpack)/2):]
-  commonLetter = ''.join(set(firstCompartment)&set(secondCompartment)).swapcase()
-  return  ord(commonLetter)-64 if ord(commonLetter) <= 90 else ord(commonLetter)-70
+  commonLetter = ''.join(set(firstCompartment)&set(secondCompartment))
+  return  ord(commonLetter)-38 if ord(commonLetter) <= 90 else ord(commonLetter)-96
 
 def GetPrioritySum(backpacks: List[str]) -> int:
   return sum([GetCommonLetterPriority(backpack) for backpack in backpacks])
 
+def GetGroupsSum(backpacks: List[str]) -> int:
+  totalSum = 0
+  for i in range(0, len(backpacks), 3):
+    group = backpacks[i:i+3]
+    commonLetter = ''.join(set(group[0])&set(group[1])&set(group[2]))
+    totalSum += ord(commonLetter)-38 if ord(commonLetter) <= 90 else ord(commonLetter)-96
+  return totalSum
+
 if __name__ == "__main__":
   backpacks = pathlib.Path("puzzle-input\input_day3.txt").read_text().split("\n")[:-1]
-  print(GetPrioritySum(backpacks))
+  print(f"{GetPrioritySum(backpacks)=}")
+  print(f"{GetGroupsSum(backpacks)=}")
